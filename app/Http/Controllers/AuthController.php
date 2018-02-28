57b9cebc0bf3d1fea2e20320d9e99f34aaabd496
samjbro <sjbroughton1@gmail.com>
2018-02-28 22:20:55 +0000

    initial commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserLoginRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login']]);
+    }
+
+    public function login(UserLoginRequest $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (! $token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+}
---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Product;
+use Faker\Generator as Faker;
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'name' => ucwords($faker->words(random_int(1, 4), true)),
+        'desc1' => $faker->sentence,
+        'manufacturer_part_no' => $faker->randomNumber(8),
+        'price' => $faker->numberBetween(500,100000)
+    ];
+});
